f2e491a51c4c6fc09029371c1c928455faf67db5
Nariman <narimanhasanpanah@gmail.com>
2026-07-05 21:05:54 +0330

    Style(Home) : add gradiant background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <div
+        id="task-manager-page"
+        className="min-h-screen bg-linear-to-br from-cyan-200 via-white to-emerald-200 p-4 md:p-8"
+      ></div>
+    </>
+  );
 };
 
 export default App;
