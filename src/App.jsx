c35d931a3fe222d121a3c15f81962b263bc15c84
Nariman <narimanhasanpanah@gmail.com>
2026-07-05 22:40:33 +0330

    Feat : add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
       <div
         id="task-manager-page"
         className="min-h-screen bg-linear-to-br from-cyan-200 via-white to-emerald-200 p-4 md:p-8"
-      ></div>
+      >
+        <div id="task-manager" className="max-w-5xl mx-auto">
+          <Header />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div id="header" className="text-center mb-8">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl text-black font-bold mb-2 drop-shadow-gray-900">
+        Task Manager
+      </h1>
+      <p className="text-black text-sm sm:text-base md:text-lg">
+        Organize your life, one task at a time
+      </p>
+    </div>
+  );
+};
+
+export default Header;
