d7224db0e5bcb8ac51924c60c408fe087e3848fe
Nariman <narimanhasanpanah@gmail.com>
2026-07-07 14:13:13 +0330

    Feat: add addTask section & chore: install lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import StatsBar from "./components/StatsBar";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -12,6 +13,10 @@ const App = () => {
     starred: tasks.filter((t) => t.starred).length,
   };
 
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
       <div
@@ -21,6 +26,12 @@ const App = () => {
         <div id="task-manager" className="max-w-5xl mx-auto">
           <Header />
           <StatsBar stats={stats} />
+          <div
+            id="main"
+            className="bg-gray-500/20 backdrop-blur-xl rounded-2xl shadow-2xl border border-gray-700 overflow-hidden"
+          >
+            <AddTask onAddTask={handleAddTask} />
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Plus } from "lucide-react";
+
+const AddTask = ({ onAddTask }) => {
+  const [taskInput, setTaskInput] = useState("");
+
+  const handleAdd = () => {
+    if (taskInput.trim() === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      text: taskInput,
+      completed: false,
+      createdAt: new Date().toISOString(),
+      starred: false,
+    };
+
+    onAddTask(newTask);
+    setTaskInput("");
+  };
+
+  return (
+    <>
+      <div
+        id="add-task-container"
+        className="p-6 bg-linear-to-rbg-linear-to-br from-cyan-200 via-white to-emerald-200"
+      >
+        <div className="flex flex-col md:flex-row gap-3 mb-4">
+          <input
+            type="text"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && handleAdd()}
+            placeholder="What needs to be done?"
+            className="flex-1 px-4 py-3 bg-gray-200 backdrop-blur-md border border-gray-500 rounded-xl text-black placeholder-gray-500 focus:outline-none"
+          />
+          <button
+            onClick={handleAdd}
+            className="px-6 py-3 bg-linear-to-r from-cyan-200 via-white to-emerald-200 text-gray-700 border border-gray-400 rounded-2xl font-semibold hover:from-cyan-400 hover:via-white hover:to-emerald-400 shadow-lg hover:shadow-xl active:scale-95 flex items-center justify-center gap-2 cursor-pointer transition-all"
+          >
+            <Plus size={20} />
+            Add Task
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddTask;
