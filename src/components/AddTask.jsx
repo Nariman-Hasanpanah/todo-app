aca43dd06af0409978545275db8c2cb25b28c3e0
Nariman <narimanhasanpanah@gmail.com>
2026-07-07 19:41:26 +0330

    Feat(AddTask) : add priority section and category  section

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { Plus } from "lucide-react";
+import { categories } from "../data/categories";
 
 const AddTask = ({ onAddTask }) => {
   const [taskInput, setTaskInput] = useState("");
+  const [priority, setPriority] = useState("medium");
+  const [category, setCategory] = useState("personal");
 
   const handleAdd = () => {
     if (taskInput.trim() === "") return;
@@ -11,6 +14,8 @@ const AddTask = ({ onAddTask }) => {
       id: Date.now(),
       text: taskInput,
       completed: false,
+      priority: priority,
+      category: categories,
       createdAt: new Date().toISOString(),
       starred: false,
     };
@@ -41,6 +46,34 @@ const AddTask = ({ onAddTask }) => {
             <Plus size={20} />
             Add Task
           </button>
+        </div>
+        <div className="flex flex-wrap gap-5">
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="px-4 py-2 bg-gray-300 border border-gray-400 rounded-xl text-gray-800 focus:outline-none"
+          >
+            {Object.entries(categories).map(([key, cat]) => (
+              <option key={key} value={key} className="bg-gray-400/50">
+                {cat.icon} {cat.name}
+              </option>
+            ))}
+          </select>
+          <select
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}
+            className="px-4 py-2 bg-gray-300 border border-gray-400 rounded-xl text-gray-800 focus:outline-none"
+          >
+            <option value="low" className="bg-gray-400/50">
+              🟢 Low Priority
+            </option>
+            <option value="medium" className="bg-gray-400/50">
+              🟡 Medium Priority
+            </option>
+            <option value="high" className="bg-gray-400/50">
+              🔴 High Priority
+            </option>
+          </select>
         </div>
       </div>
     </>
---
+++ src/data/categories.jsx
@@ -0,0 +1,7 @@
+export const categories = {
+  personal: { name: "Personal", color: "bg-blue-500", icon: "👤" },
+  work: { name: "Work", color: "bg-purple-500", icon: "💼" },
+  shopping: { name: "Shopping", color: "bg-green-500", icon: "🛒" },
+  health: { name: "Health", color: "bg-red-500", icon: "❤️" },
+  study: { name: "Study", color: "bg-yellow-500", icon: "📚" },
+};
